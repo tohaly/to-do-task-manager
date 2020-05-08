377fc09c5f758b0133c280f9347ab9b15bcdf68c
Anton Voitenko <voytenko.ad@yandex.ru>
2020-05-09 04:09:45 +0800

    Added custom checkbox

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="task__item">
-    <input
-      class="task__checkbox"
-      v-if="!isNewPage"
-      type="checkbox"
-      @click="changeCheckBox(index)"
-      v-model="isChecked"
-    />
-    <div class="task__item-text" v-if="!isEditing">{{ text }}</div>
-    <input
-      type="text"
-      class="task__input"
-      placeholder="Enter a task"
-      v-if="isEditing"
-      v-model="textHolder"
-      v-on:keyup.enter="keyEditTaskHandler"
-      @input="validate()"
-      minlength="2"
-      maxlength="25"
-    />
+    <label class="task__label">
+      <input
+        id="task-input-chekcbox"
+        class="task__checkbox"
+        v-if="!isNewPage"
+        type="checkbox"
+        @click="changeCheckBox(index)"
+        v-model="isChecked"
+      />
+      <div class="task__custom-checkbox"></div>
+      <div class="task__item-text" v-if="!isEditing">{{ text }}</div>
+      <input
+        type="text"
+        class="task__input"
+        placeholder="Enter a task"
+        v-if="isEditing"
+        v-model="textHolder"
+        v-on:keyup.enter="keyEditTaskHandler"
+        @input="validate()"
+        minlength="2"
+        maxlength="25"
+      />
+    </label>
     <Button
       :class="[{ task__button_disabled: isButtonDisabled }, 'task__button']"
       :action="isEditing ? saveChangedTask : showEditField"
@@ -42,7 +46,7 @@ export default {
     isNewPage: Boolean,
     id: String,
     saveTask: Function,
-    deleteTask: Function,
+    deleteTask: Function
   },
   components: { Button },
   data() {
@@ -50,7 +54,7 @@ export default {
       isEditing: false,
       buttonName: "edit",
       isButtonDisabled: false,
-      textHolder: this.text,
+      textHolder: this.text
     };
   },
   methods: {
@@ -71,8 +75,8 @@ export default {
       if (!this.isButtonDisabled) {
         this.saveChangedTask();
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -80,6 +84,13 @@ export default {
 .task__wrapper {
   width: 100%;
   display: flex;
+}
+
+.task__label {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  position: relative;
 }
 
 .task__item {
@@ -91,30 +102,30 @@ export default {
 }
 
 .task__checkbox {
-  display: block;
-  width: 20px;
-  height: 20px;
-  background-color: transparent;
+  position: absolute;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+}
+
+.task__custom-checkbox {
+  position: relative;
+  width: 22px;
+  height: 22px;
+  margin: auto 6px auto 0;
   border-radius: 50%;
-  border: 3px solid #373f41;
-  opacity: 0.8;
-  appearance: none;
-  outline: none;
-  cursor: pointer;
-  align-self: center;
-  padding: 8px;
-  margin-right: 16px;
+  border: 3px solid #818586;
+}
+
+.task__checkbox:checked + .task__custom-checkbox {
+  background-color: transparent;
+  border: none;
+  background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 22 22'%3e%3cdefs/%3e%3cpath fill='%23818586' d='M11 0C4.9348 0 0 4.9344 0 11c0 6.0657 4.9348 11 11 11s11-4.9343 11-11c0-6.0656-4.9348-11-11-11zm6.1325 7.3315l-6.7692 7.6154a.846.846 0 01-1.1614.0986l-4.2308-3.3847c-.3646-.2915-.4239-.8241-.132-1.1892.2915-.3651.8246-.4244 1.1893-.1324l3.6034 2.8824 6.2357-7.015c.3097-.3495.8453-.3808 1.1943-.0703.3495.311.3812.8457.0707 1.1952z'/%3e%3c/svg%3e");
 }
 
 .task__checkbox:checked ~ .task__item-text {
   text-decoration: line-through;
-  color: #ccc;
+  opacity: 0.6;
 }
-
-.task__checkbox:checked {
-  background-color: #373f41;
-}
-
 .task__item-text {
   display: block;
   font-size: 30px;
